f855887f3ebc62ed8e17c1ce5d8e4b8fe6d627e2
micahcantor <42819290+micahcantor@users.noreply.github.com>
2020-04-15 22:56:34 -0400

    Add files via upload

--- content.js
+++ content.js
@@ -1,17 +1,21 @@
+listenAndLoad();
 
-window.addEventListener("load", function() {
-    var btn = document.createElement("BUTTON");
-    var t = document.createTextNode("this is button");
-    btn.appendChild(t);
-    var form = document.getElementsByTagName("form")[0];
-    var main = document.getElementById("ow43");
-    main.insertBefore(form, main.children[1]);
-});
+function listenAndLoad() {
+    window.addEventListener("load", function() {
+        var form = document.getElementsByTagName("form")[0];
+        var main = document.getElementById("ow43");
+        main.insertBefore(form, main.children[1]);
+    
+        form.addEventListener('submit', event => {
+            event.preventDefault();
+            // search code here
+            console.log('submit');
+          });
+    });
+}
+
+
 
 
 
     
-
-
-// Find parent div for elements
-// try adding a copy of the "share something with your class" element
--- start.js
+++ start.js
@@ -1,38 +1,50 @@
-var s = document.createElement('script');
-s.src = chrome.runtime.getURL('content.js');
+addFormStyle();
+addFormScript();
+addFormHTML();
 
-var form = document.createElement('form')
-var input = document.createElement('input')
-var btn = document.createElement('button')
-var searchIcon = document.createElement('i')
-form.setAttribute("class", "example")
-btn.setAttribute("type", "submit")
-searchIcon.setAttribute("class", "fa fa-search")
-Object.assign(input, {
-    type : "text",
-    placeholder: "Search the stream",
-    name : "search"
-})
-btn.appendChild(searchIcon)
-form.appendChild(input)
-form.appendChild(btn)
+function addFormHTML () {
+    var form = document.createElement('form');
+    var input = document.createElement('input');
+    var btn = document.createElement('button');
+    var searchIcon = document.createElement('i');
 
-var styleLink = document.createElement("link")
-styleLink.setAttribute("rel", "stylesheet")
-styleLink.setAttribute("href", chrome.runtime.getURL('searchStyle.css'))
+    form.setAttribute("class", "example");
+    form.setAttribute("action", "/contact");
+    form.setAttribute("method", "POST");
+    btn.setAttribute("type", "submit");
+    searchIcon.setAttribute("class", "fa fa-search");
+    Object.assign(input, {
+        type : "text",
+        placeholder: "Search the stream",
+        name : "search"
+    });
 
-var imgStyle = document.createElement("link")
-imgStyle.setAttribute("rel", "stylesheet")
-imgStyle.setAttribute("href", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css")
+    btn.appendChild(searchIcon);
+    form.appendChild(input);
+    form.appendChild(btn);
+    document.body.appendChild(form);
+}
+function addFormStyle() {
+    var styleLink = document.createElement("link");
+    styleLink.setAttribute("rel", "stylesheet");
+    styleLink.setAttribute("href", chrome.runtime.getURL('searchStyle.css'));
 
-console.log(form)
-console.log(s)
-console.log(styleLink)
-s.onload = function() {
-    this.remove();
-};
+    var imgStyle = document.createElement("link");
+    imgStyle.setAttribute("rel", "stylesheet");
+    imgStyle.setAttribute("href", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css");
 
-document.head.appendChild(styleLink)
-document.head.appendChild(imgStyle)
-document.body.appendChild(s);
-document.body.appendChild(form)
+    document.head.appendChild(styleLink);
+    document.head.appendChild(imgStyle);
+}
+function addFormScript () {
+    var content = document.createElement('script');
+    content.src = chrome.runtime.getURL('content.js');
+    content.onload = function() {
+        this.remove();
+    };
+    document.body.appendChild(content);
+}
+
+
+
+
