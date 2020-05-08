cdef4a66ac080bdabe4b80ca3e2d20f25feac232
Micah Cantor <micahcantor01@gmail.com>
2020-05-08 00:15:02 -0400

    Overhauled authorization to work silently

--- background.js
+++ background.js
@@ -1,50 +1,151 @@
 
-var email = null;
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse){
-    if (request.message == "userEmail") {
+// When user presses the search button: 
+  // try to use access token from storage
+  // if authorization error, handle it:
+    // check if there is a refresh token in storage
+      // if so, exchange for access token and use that
+    // else launch oauth2 interactive
+      // exchange code for fresh tokens
+      // save refresh and access tokens to storage
+      // use access token
+
+onSearch();
+
+function onSearch () {
+  chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+    var email = null;
+    if (request.message == "userEmail") {                                       // runs on page load and sends the user's email
       email = request.email;
-      getAuthTokenSilent(email);
-      sendResponse(email);
-    } 
-    else {
-       chrome.identity.getAuthToken({interactive: true}, function (token) {
-        if (chrome.runtime.lastError) {
-          console.log(chrome.runtime.lastError.message);
-          getAuthTokenSilent(email);
-          return
-        }
-    
-        const myHeaders = new Headers({
-          "Authorization": "Bearer " + token,
+      sendResponse("email set");
+    } else {                                                                    // message from search button press
+      (async function tokenFlow() {                                             // container function since the addListener callback can't be async
+        const token_response = await getToken("access");
+        var access_token = await token_response.access
+        var myHeaders = new Headers({
+          "Authorization": "Bearer " + access_token,
           "Accept": "application/json"
-        })
+        });
+
         fetch(request.url, {headers: myHeaders})
-          .then(response => response.json())
-          .then(function(data) {
-            if (request.type == "courseID") {
-              sendResponse(getCourseID(data, request));
+          .then(function (response) {
+            console.log(response.status)
+            if (response.status == 401) throw new Error("authorization error");
+            return response.json()
+          })
+          .then(function (data) {
+            console.log("access token used... success")
+            sendResponse(cleanData(request, data));
+          })
+          .catch(async function (error) {
+            console.log("no access token... catching")
+            const token_response = await getToken("refresh");
+            const refresh_token = await token_response.refresh;
+            if (refresh_token != null) {                                          // refresh token found in storage
+              console.log("refresh token found")
+              access_token = await refreshAccess(refresh_token);                  // update access token with refresh token
+              saveTokens(refresh_token, access_token)                             // update storage with new tokens
+              myHeaders.set("Authorization", "Bearer " + access_token);         
+              response = await fetch(request.url, {headers: myHeaders});          // resend GET request with updated access token
+              data = await response.json();
+              sendResponse(cleanData(request, data));
             }
-            else if (request.type == "assignments") {
-              sendResponse(getAssignments(data));
-            }
-            else if (request.type == "announcements") {
-              sendResponse(getAnnouncements(data, request));
+            else {                                                                // no refresh token found in storage
+              console.log("no refresh token found")
+              access_token = await oauth2(email, true);// gets and saves new tokens from oauth2 interactive
+              myHeaders.set("Authorization", "Bearer " + access_token);         
+              response = await fetch(request.url, {headers: myHeaders});          // resend GET request with updated access token
+              data = await response.json();
+              console.log(data)
+              sendResponse(cleanData(request, data));
             }
           })
-      })
-      return true;
+      })()
     }
-})
-
-function getAuthTokenSilent(email) {
-  chrome.identity.getAuthToken({account: {id: email}}, function(token) {
-    if (chrome.runtime.lastError) {
-      console.log(chrome.runtime.lastError.message)
-      return
-    }
-    console.log(token)
+    return true; // return true in the message listener callback to make it async
   })
+}
+
+function cleanData (request, data) {
+  if (request.type == "courseID") {
+    return getCourseID(data, request);
+  }
+  else if (request.type == "assignments") {
+    return getAssignments(data);
+  }
+  else if (request.type == "announcements") {
+    return getAnnouncements(data, request);
+  }
+}
+
+function oauth2(email, interactive) {
+  var auth_url = "https://accounts.google.com/o/oauth2/auth?";
+  const client_id = "809411372636-42mpeh1d7ntk8vor0kuhtsg66ug1olcd.apps.googleusercontent.com";
+  const redirect_uri = "https://hkipfjomcdcmllhnkpmbndggbgdicmic.chromiumapp.org/oauth2";
+
+  const auth_params = {
+    client_id: client_id,
+    redirect_uri: redirect_uri,
+    response_type: "code",
+    access_type: "offline",
+    prompt: "consent",
+    scope: "https://www.googleapis.com/auth/classroom.courses.readonly https://www.googleapis.com/auth/classroom.coursework.me https://www.googleapis.com/auth/classroom.announcements.readonly",
+    login_hint: email
+  }
+
+  const url = new URLSearchParams(Object.entries(auth_params));
+  url.toString();
+  auth_url += url;
+
+  return new Promise (function(resolve, reject) {
+    chrome.identity.launchWebAuthFlow({url: auth_url, interactive: interactive}, async function(response) {
+      const code = response.slice(response.indexOf("=") + 1, response.indexOf("&"))
+      token = await exchangeCode(code, client_id, redirect_uri);
+      resolve(token)
+    })
+  })
+}
+
+async function exchangeCode(code, client_id, redirect_uri) {
+  const url = 'https://www.googleapis.com/oauth2/v4/token'
+  const client_secret = "FOVBbXcMD84uFzIHpYvNOBOk";
+  const body ="code=" + code + "&" +
+              "client_id=" + client_id + "&" +
+              "client_secret=" + client_secret + "&" +
+              "redirect_uri=" + redirect_uri + "&" + 
+              "grant_type=authorization_code"
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: body
+  })
+
+  const json = await response.json();
+  saveTokens(json.refresh_token, json.access_token);
+  return json.access_token
+}
+
+async function refreshAccess(refresh_token) {
+  const url = "https://oauth2.googleapis.com/token";
+  const client_id = "809411372636-42mpeh1d7ntk8vor0kuhtsg66ug1olcd.apps.googleusercontent.com";
+  const client_secret = "FOVBbXcMD84uFzIHpYvNOBOk";
+  const body ="client_id=" + client_id + "&" +
+              "client_secret=" + client_secret + "&" +
+              "refresh_token=" + refresh_token + "&" + 
+              "grant_type=refresh_token"
+  
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: body
+  })
+  const json = await response.json();
+  const token = await json.access_token;
+  return token
 }
 
 function getCourseID(data, request) {
@@ -61,7 +162,6 @@ function getAssignments(data) {
   var courseWorkValues = [];
   if (courseWork.length > 0) {
     var descriptions = [];
-    console.log(data)
     for (let i = 0; i < courseWork.length; i++) { 
         if (courseWork[i].hasOwnProperty("description")) {     
             descriptions.push(courseWork[i].description);
@@ -105,3 +205,22 @@ function getAnnouncements(data, request) {
   }
   return courseWorkValues;
 }
+
+function saveTokens(refresh_token, access_token) {
+  chrome.storage.sync.set({
+    'refresh': refresh_token,
+    'access': access_token
+  }, 
+  function callback() {
+    console.log("refresh token stored")
+  })
+}
+
+function getToken(type) {
+  return new Promise(function(resolve, reject) {
+    chrome.storage.sync.get([type], function (result) {
+      resolve(result)
+    })
+  })
+}
+
